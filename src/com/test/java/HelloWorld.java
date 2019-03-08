41804a34999fbb6feaca2f6fac406ff6d35c9c91
qianjingyan <3270504160@qq.com>
2019-03-08 14:48:00 +0800

    Initial commit

---
+++ src/com/test/java/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.test.java;
+
+public class HelloWorld {
+
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println();
+        System.out.println("args = " + args);
+        System.out.println("HelloWorld.main");
+        System.out.println("args = " + args);
+        System.out.println("args = [" + args + "]");
+
+
+    }
+
+}
